cc781d44a1dc4ac624d3d627ee94bff182a291e4
MagdalenaOuirane <magdalenaouirane@gmail.com>
2019-09-16 23:10:52 +0200

    add an error to element form

--- src/Components/RegistrationForm.js
+++ src/Components/RegistrationForm.js
@@ -96,7 +96,7 @@ export class RegistrationForm extends React.Component {
 
                     </label>
 
-                    <label htmlFor="accept"><input type="checkbox" id="accept" name="accept" checked={this.state.accept} onChange={this.handleChange} />You agree to the website User Agreement,Privacy Policy and Cookie policy</label>
+                    <label htmlFor="accept"><input type="checkbox" id="accept" name="accept" checked={this.state.accept} onChange={this.handleChange} />{this.state.errors && <span>{this.messages.accept_incorrect}</span>}You agree to the website User Agreement,Privacy Policy and Cookie policy</label>
 
 
                     <button >JOIN</button>
