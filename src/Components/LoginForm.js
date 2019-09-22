bbd2836907d7df7f42934b57d6329b4ecea9a249
MagdalenaOuirane <magdalenaouirane@gmail.com>
2019-09-22 22:20:13 +0200

    add button and style

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -18,6 +18,13 @@ export class LoginForm extends React.Component {
                     </label>
 
                     <label htmlFor="password">Password<input type="password" id="password" value={this.state.password} on Change={this.handleChange} /></label>
+
+
+                    <button>Sign in</button>
+
+                    <p className="password">Forgot password?</p>
+
+                    <p className="web">New to this website? Join now</p>
                 </form>
 
             </div>
