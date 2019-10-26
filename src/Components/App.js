2c071b1befb48b91efca5673bba58c3b87ffa105
MagdalenaOuirane <magdalenaouirane@gmail.com>
2019-10-27 01:44:50 +0200

    import  file HomePage

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,6 +6,8 @@ import LoginForm from './LoginForm';
 import './LoginForm.css';
 import Notfound from './Notfound';
 import './Notfound.css';
+import HomePage from './HomePage';
+import './HomePage.css';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
 
@@ -17,6 +19,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/" component={LoginForm} />
             <Route path="/registrationform" component={RegistrationForm} />
+            <Route path="/homepage" component={HomePage} />
             <Route component={Notfound} />
           </Switch>
 
