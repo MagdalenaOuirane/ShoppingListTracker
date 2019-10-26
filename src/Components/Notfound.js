4dda314081c40a79b4fc017798bfc9675c56aba7
MagdalenaOuirane <magdalenaouirane@gmail.com>
2019-10-26 22:54:48 +0200

    change not found view

--- src/Components/Notfound.js
+++ src/Components/Notfound.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const Notfound = () =>
 
-    <h1 style={{ position: 'absolute', top: '0', left: '0' }}>Not found</h1>
+
+const Notfound = () => {
+
+
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>It seems page you are looking for no longer exists.</h1>
+            <img src={require('./error.jpg')} alt="" />
+        </div>
+    );
+};
+
+// <h1 style={{ position: 'absolute', top: '0', left: '0' }}>Not found</h1>
 
 
 
