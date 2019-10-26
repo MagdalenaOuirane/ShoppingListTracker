07e53faa0bebb76b072d751facdf2bdd0e27ceeb
MagdalenaOuirane <magdalenaouirane@gmail.com>
2019-10-27 01:46:35 +0200

    create HomePage.js and .css file

---
+++ src/Components/HomePage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+export class HomePage extends React.Component {
+    render() {
+        return (
+            <h1>Hello Home Page!!</h1>
+        );
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default HomePage;
